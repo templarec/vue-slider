8191ac1bb7df295cc7b0af2a5cf0691fdd5031b0
Lorenzo Bernini <templarec@gmail.com>
2021-03-24 16:52:27 +0100

    Esercizio completato

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+console.log('--- Script loading ---');
+//start app vue
+var app = new Vue (
+  {
+    el: "#root",
+    data: {
+      titolo: "Show room felino",
+      index: 0,
+      images : [
+        "./assets/img/bengal.png",
+        "./assets/img/mainecoon.png",
+        "./assets/img/scottish.png",
+        "./assets/img/sphynx.jpg"
+      ]
+    },
+    methods: {
+      next: function () {
+        if (this.index < this.images.length -1) {
+          this.index++;
+        } else {
+          this.index = 0;
+        }
+      },
+      prev: function () {
+        if (this.index > 0) {
+          this.index--;
+        } else {
+          this.index = this.images.length -1;
+        }
+      },
+      dotSel: function(index) {
+        this.index = index;
+      }
+    }
+  }
+);
